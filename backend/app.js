56a80a1cde64105dce426099c0c0052121c053e7
Christoph Loy <cloy202@gmail.com>
2020-05-24 18:41:35 +0200

    simplyfied app.js

--- backend/app.js
+++ backend/app.js
@@ -8,20 +8,8 @@ const port = 3000;
 
 app.use(express.json());
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('/', (req, res, next) => {
+  res.json({message: "This api is working"})
+})
 
 app.listen(3000, () => console.log(`Server is running on port 3000`));
