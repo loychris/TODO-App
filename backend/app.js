27272a23fc8e81a08088dd87ba3e209d1678a91c
Chris Loy <cloy202@gmail.com>
2020-05-25 17:21:32 +0200

    Added getAll & deleteTodo route

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,15 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-  res.json({message: "This api is working"})
+app.get('/', async (req, res, next) => {
+  let todos; 
+  try{
+    todos = await Todo.find({});
+  }catch(err){
+    console.log(err);
+  }
+  console.log("Fetched all todos");
+  res.json(todos);
 })
 
 app.post('/todo', async (req, res, next) => {
@@ -23,16 +30,27 @@ app.post('/todo', async (req, res, next) => {
     progress
   })
   try{
-    await createdTodo.save();
+    await createdTodo.save(  () => console.log("created todo"));
   }catch(err){
     console.log(err);
   }
   res.status(201).json(createdTodo);
 })
 
+app.delete('/todo/:id', async (req, res, next) => {
+  const id = req.params.id;
+  let todo;
+  try{
+    todo = await Todo.findByIdAndDelete(id, () => console.log("deleted todo"));
+  }catch(err){
+    console.log(err); 
+  }
+  res.json({message: "Deleted Successfully"});
+})
+
 mongoose
   .connect(
-    "mongodb+srv://chris:eKMMU5IkPnP2BSDo@cluster0-qsimx.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://chris:Uh30ggX40gR86kXU@cluster0-qsimx.mongodb.net/test?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
